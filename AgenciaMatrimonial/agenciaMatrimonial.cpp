d6879f73369ac40d3866bd78654ddc361b12d8e8
jupmorenor@gmail.com <jupmorenor@gmail.com@8fb1dd53-c1c5-0ca3-83e5-e1b4869c213a>
2014-11-12 14:45:04 +0000

    posible constructor de la multilista

--- AgenciaMatrimonial/agenciaMatrimonial.cpp
+++ AgenciaMatrimonial/agenciaMatrimonial.cpp
@@ -1,9 +1,11 @@
-/*
+/**
  * agenciaMatrimonial.cpp
  *
  *  Created on: 9/11/2014
- *      Author: Juanpa y Yami
+ *      Author: Juan pablo Moreno Rico - 20111020059
+ *      Author: Diana Cristhina Pérez Pérez - 20111020069
  */
+
 #include "agenciaMatrimonial.h"
 
 const std::string LISTAS[] = {"SEXO", "EDAD", "NVACADEMICO", "COMPLEXION", "ESTATURA"};
@@ -13,6 +15,8 @@ const std::string EDAD[] = {"19A24", "25A35", "36A45", "46A60", "60MAS"};
 const std::string NVACADEMICO[] = {"BACHILLER", "TECNICO", "PROFESIONAL", "ESPECIALIZACION", "MAESTRIA", "DOCTORADO"};
 const std::string COMPLEXION[] = {"GRUESA", "NORMAL", "DELGADA"};
 const std::string ESTATURA[] = {"MENOS150", "151A160", "161A170", "171A180", "180MAS"};
+const int TAMS = {2, 5, 6, 3, 5};
+const int CANT = 5;
 
 template <class T> Nodo<T>* AgenciaMatrimonial<T>::crear_nodo(std::string id) {
 	Nodo<T> *nodo = new Nodo<T>;
@@ -31,7 +35,19 @@ template <class T> subNodo<T>* AgenciaMatrimonial<T>::crear_subnodo(std::string 
 
 template <class T> AgenciaMatrimonial<T>::AgenciaMatrimonial() {
 	cabeza = crear_nodo(LISTAS[0]);
-	//TODO
+	Nodo<T> *aux1 = cabeza;
+	subNodo<T> *aux2;
+	for (int i=1; i<CANT; i++) {
+
+		aux1->sublista = crear_subnodo(SUBLISTAS[i-1][0]);
+		aux2 = aux1->sublista;
+		for (int j=1; j<TAMS[i]; j++) {
+			aux2->siguiente = crear_subnodo(SUBLISTAS[i-1][j]);
+			aux2 = aux2->siguiente;
+		}
+		aux1->siguiente = crear_nodo(LISTAS[i]);
+		aux1 = aux1->siguiente;
+	}
 }
 
 template <class T> AgenciaMatrimonial<T>::~AgenciaMatrimonial() {
