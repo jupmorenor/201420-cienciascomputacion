9f336f488a491f70522e23c101ae64e924f90e9c
jupmorenor@gmail.com <jupmorenor@gmail.com@8fb1dd53-c1c5-0ca3-83e5-e1b4869c213a>
2014-11-22 21:33:44 +0000

    Uso de <string> desde el espacio de nombres. Preparacion de los metodos de Admin y user para ser implementados.

--- AgenciaMatrimonial/admin.cpp
+++ AgenciaMatrimonial/admin.cpp
@@ -2,9 +2,38 @@
  * admin.cpp
  *
  *  Created on: 18/11/2014
- *      Author: -
+ *      Author: Juan pablo Moreno Rico - 20111020059
+ *      Author: Diana Cristhina Pérez Pérez - 20111020069
  */
 
 #include "admin.h"
 
+Admin::Admin() {
+}
 
+Admin::~Admin() {
+}
+
+void Admin::inicializar() {
+}
+
+void Admin::guardarInformacion() {
+}
+
+bool Admin::registrarNuevoUsuario() {
+}
+
+bool Admin::eliminarUsuarioRegistrado() {
+}
+
+void Admin::mostrarPorEdadesSegunCiudad(string ciudad) {
+}
+
+void Admin::mostrarPorHijosyCiudad(int hijos) {
+}
+
+void Admin::mostrarPorSexoyEdad(char sexo, int edad) {
+}
+
+void Admin::mostrarTodosPorCiudad(string ciudad) {
+}
