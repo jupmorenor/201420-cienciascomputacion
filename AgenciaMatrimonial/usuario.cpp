37de02344438e2cfa7fb9bee210b0c42d93cef49
jupmorenor@gmail.com <jupmorenor@gmail.com@8fb1dd53-c1c5-0ca3-83e5-e1b4869c213a>
2014-11-30 17:25:51 +0000

    Se corrige un error de rango en el constructor de la multilista. Se agrega un archivo cpp donde se define (diferente de la declaracion) la multilista como atributo estatico de las clases. Ya no genera errores de ejecucion.

---
+++ AgenciaMatrimonial/usuario.cpp
@@ -0,0 +1,11 @@
+/*
+ * usuario.cpp
+ *
+ *  Created on: 30/11/2014
+ *      Author: Juanpa y Yami
+ */
+#include "usuario.h"
+
+template <class T> AgenciaMatrimonial<T> *Usuario<T>::agencia;
+template class Usuario<Afiliado>;
+
